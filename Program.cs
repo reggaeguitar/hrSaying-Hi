2c26088839aadd5f382e18c1e7acc4b650c4e5c6
Joseph Brady <escalade2005@hotmail.com>
2015-04-25 19:42:39 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace hrSaying_Hi
+{
+    class Program
+    {
+        static void Main()
+        {
+            var regex = new Regex(@"^[Hh][Ii] [^dD]", RegexOptions.Compiled);
+            var numCases = Int32.Parse(Console.ReadLine());
+            while (numCases-- > 0)
+            {
+                var str = Console.ReadLine();
+                if (regex.IsMatch(str))
+                    Console.WriteLine(str);
+            }
+        }
+    }
+}
